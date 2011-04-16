4ea8413ba8fcc98da7b836e3bff1f0ed0d558233
Ryan Lake <ryan@tmrlake.com>
2011-04-16 14:47:25 -0600

    rx_test.cpp is a working multichannel receiver

---
+++ aoa/rx_test.cpp
@@ -0,0 +1,154 @@
+//
+// Copyright 2010-2011 Ettus Research LLC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/usrp/subdev_spec.hpp>
+#include <uhd/types/ref_vector.hpp>
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
+#include <iostream>
+#include <fstream>
+#include <complex>
+
+namespace po = boost::program_options;
+
+int UHD_SAFE_MAIN(int argc, char *argv[]){
+    uhd::set_thread_priority_safe();
+
+    //variables to be set by po
+    std::string args, file;
+    size_t total_num_samps;
+    double rate, freq, gain;
+
+    //setup the program options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "help message")
+        ("args", po::value<std::string>(&args)->default_value(""), "multi uhd device address args")
+        ("file", po::value<std::string>(&file)->default_value("out.16sc.dat"), "name of the file to write binary samples to")
+        ("nsamps", po::value<size_t>(&total_num_samps)->default_value(1000), "total number of samples to receive")
+        ("rate", po::value<double>(&rate)->default_value(100e6/16), "rate of incoming samples")
+        ("freq", po::value<double>(&freq)->default_value(0), "rf center frequency in Hz")
+        ("gain", po::value<double>(&gain)->default_value(0), "gain for the RF chain")
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    //print the help message
+    if (vm.count("help")){
+        std::cout << boost::format("UHD RX samples to file %s") % desc << std::endl;
+        return ~0;
+    }
+
+    //create a usrp device
+    std::cout << std::endl;
+    std::cout << boost::format("Creating the usrp device with: %s...") % args << std::endl;
+    uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(args);
+    usrp->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("A: B:"), 0);
+    std::cout << boost::format("Using Device: %s") % usrp->get_pp_string() << std::endl;
+    
+    
+    //set the rx sample rate
+    std::cout << boost::format("Setting RX Rate: %f Msps...") % (rate/1e6) << std::endl;
+    usrp->set_rx_rate(rate);
+    std::cout << boost::format("Actual RX Rate: %f Msps...") % (usrp->get_rx_rate()/1e6) << std::endl << std::endl;
+
+    //set the rx center frequency
+    std::cout << boost::format("Setting RX Freq: %f Mhz...") % (freq/1e6) << std::endl;
+    usrp->set_rx_freq(freq);
+    std::cout << boost::format("Actual RX Freq: %f Mhz...") % (usrp->get_rx_freq()/1e6) << std::endl << std::endl;
+
+    //set the rx rf gain
+    std::cout << boost::format("Setting RX Gain: %f dB...") % gain << std::endl;
+    usrp->set_rx_gain(gain);
+    std::cout << boost::format("Actual RX Gain: %f dB...") % usrp->get_rx_gain() << std::endl << std::endl;
+
+    boost::this_thread::sleep(boost::posix_time::seconds(1)); //allow for some setup time
+
+    //setup streaming
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE);
+    stream_cmd.num_samps = total_num_samps;
+    stream_cmd.stream_now = true;
+    usrp->issue_stream_cmd(stream_cmd);
+
+    std::cout << usrp->get_pp_string();
+
+    //loop until total number of samples reached
+    size_t num_acc_samps = 0; //number of accumulated samples
+    uhd::rx_metadata_t md;
+    //std::vector<std::complex<float> > buff(usrp->get_device()->get_max_recv_samps_per_packet());
+    //std::vector<std::complex<float> > buff1(usrp->get_device()->get_max_recv_samps_per_packet());
+    int maxSamps = usrp->get_device()->get_max_recv_samps_per_packet();
+
+    std::complex<short> buff[maxSamps];
+    std::complex<short> buff1[maxSamps];
+
+    std::vector<std::complex<short>* > buffers(2);
+    buffers[0] = &buff[0];
+    buffers[1] = &buff1[0];
+    for(int i = 0; i < maxSamps; i++)
+    {
+    	buff[i] = 0;
+	buff1[i] = 0;
+    }
+    std::ofstream outfile(file.c_str(), std::ofstream::binary);
+
+    while(num_acc_samps < total_num_samps){
+        size_t num_rx_samps = usrp->get_device()->recv(
+            buffers, maxSamps, md,
+           uhd::io_type_t::COMPLEX_INT16,
+            uhd::device::RECV_MODE_ONE_PACKET
+        );
+
+        //handle the error codes
+        switch(md.error_code){
+        case uhd::rx_metadata_t::ERROR_CODE_NONE:
+            break;
+
+        case uhd::rx_metadata_t::ERROR_CODE_TIMEOUT:
+            if (num_acc_samps == 0) continue;
+            std::cout << boost::format(
+                "Got timeout before all samples received, possible packet loss, exiting loop..."
+            ) << std::endl;
+            goto done_loop;
+
+        default:
+            std::cout << boost::format(
+                "Got error code 0x%x, exiting loop..."
+            ) % md.error_code << std::endl;
+            goto done_loop;
+        }
+
+        //write complex short integer samples to the binary file
+        outfile.write((const char*)&buff[0], num_rx_samps * sizeof(std::complex<short>) );
+	outfile.write("buf1", 4 * sizeof(char));
+        outfile.write((const char*)&buff1[0], num_rx_samps * sizeof(std::complex<short>));
+	outfile.write("buf2", 4 * sizeof(char));
+        num_acc_samps += num_rx_samps;
+    } done_loop:
+
+    outfile.close();
+
+    //finished
+    std::cout << std::endl << "Done!" << std::endl << std::endl;
+
+    return 0;
+}
